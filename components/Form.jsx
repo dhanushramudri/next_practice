fecfa7648a7ad297c6c2925a2caaa5a91e3a640b
dhanushramudri <ramudridhanush@gmail.com>
2024-05-28 23:28:18 +0530

    updated create-prompt page

--- components/Form.jsx
+++ components/Form.jsx
@@ -41,6 +41,18 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className="form_input"
           ></input>
         </label>
+        <div className="flex-end mx-3 my-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>{" "}
+          <button
+            type="submit"
+            disable={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   );
